205d92b4a2a3d7966b210a90c143d6bc1404f4c4
Fernando Tiberti <fertiberti@gmail.com>
2018-09-03 21:36:34 +0200

    Improve 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div class="centered">
+    <h1>Page Not Found</h1>
+    <p>Sorry, but the page you were trying to view does not exist.</p>
   </div>
 )
 
